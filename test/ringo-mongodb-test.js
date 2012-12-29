11f3a2508b0eeeee5cce712b69f36301962fa230
Fabien Meghazi <agr@amigrave.com>
2012-12-29 21:30:36 +0100

    Renamed some classes

--- test/ringo-mongodb-test.js
+++ test/ringo-mongodb-test.js
@@ -6,13 +6,13 @@ var db = mongo.connect("mongodb://localhost/testing_ringo_mongodb");
 // Connection and basic tests
 exports.testConnection = function() {
     var client = mongo.connect("mongodb://localhost");
-    var client2 = new mongo.MongoClient("localhost");
-    var client3 = new mongo.MongoClient("localhost", 27017);
+    var client2 = new mongo.Client("localhost");
+    var client3 = new mongo.Client("localhost", 27017);
 
     // test defaults
     assert.equal(client.toString(), client2.toString());
     assert.equal(client2.toString(), client3.toString());
-    assert.equal(client3.toString(), 'Mongo: localhost/127.0.0.1:27017');
+    assert.equal(client3.toString(), '[mongo.Client: localhost/127.0.0.1:27017]');
 
     // test getter
     assert.equal(client3.getVersion(), client3.version);
