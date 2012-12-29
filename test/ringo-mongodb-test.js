ad8ac655d57259826732b338c0c11ab1485e87f3
Fabien Meghazi <agr@amigrave.com>
2012-12-30 00:43:47 +0100

    Still changing names

--- test/ringo-mongodb-test.js
+++ test/ringo-mongodb-test.js
@@ -1,21 +1,29 @@
 var mongo = require("../");
 var assert = require("assert");
 
-var db = mongo.connect("mongodb://localhost/testing_ringo_mongodb");
+var dbTestName = "testing_ringo_mongodb";
+var db = mongo.connect("mongodb://localhost/" + dbTestName);
 
 // Connection and basic tests
 exports.testConnection = function() {
     var client = mongo.connect("mongodb://localhost");
-    var client2 = new mongo.Client("localhost");
-    var client3 = new mongo.Client("localhost", 27017);
+    var client2 = new mongo.MongoClient("localhost");
+    var client3 = new mongo.MongoClient("localhost", 27017);
 
     // test defaults
     assert.equal(client.toString(), client2.toString());
     assert.equal(client2.toString(), client3.toString());
-    assert.equal(client3.toString(), '[mongo.Client: localhost/127.0.0.1:27017]');
+    assert.equal(client3.toString(), '[MongoClient: localhost/127.0.0.1:27017]');
 
     // test getter
     assert.equal(client3.getVersion(), client3.version);
+
+    // Ensure test database is dropped
+    var db = client.getDB(dbTestName);
+    db.dropDatabase();
+    var databases = client.getDatabaseNames();
+    assert.isTrue(Array.isArray(databases));
+    assert.equal(databases.indexOf(dbTestName), -1);
 };
 
 if (require.main == module) {
